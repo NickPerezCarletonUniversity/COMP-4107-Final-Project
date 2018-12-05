ee4ff241b1bf4a2df57076af3359c5daaba72de8
Nick Perez <nickperez@WINDOWS2.SCS.CARLETON.CA>
2018-12-05 14:16:03 -0500

    modified kfolds for mixture of experts so that it only does kmeans on the training set

--- MixtureOfExperts/kmeans_and_data_generation.py
+++ MixtureOfExperts/kmeans_and_data_generation.py
@@ -6,7 +6,6 @@
 #https://courses.cs.ut.ee/MTAT.03.277/2014_fall/uploads/Main/deep-learning-lecture-9-combining-multiple-neural-networks-to-improve-generalization-andres-viikmaa.pdf
 import tensorflow as tf
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
 from sklearn.cluster import KMeans, MiniBatchKMeans
 
 (trX, trY), (teX, teY) = tf.keras.datasets.fashion_mnist.load_data()
@@ -18,9 +17,15 @@ trX = np.vstack((trX, teX))
 
 print(trX.shape)
 
+folds = 10
+size_of_fold = int(len(trX)/folds)
+
 answer = []
-for i in range(2,15):
-	kmeans = KMeans(n_clusters=i, random_state=0).fit(trX)
-	print(i)
-	np.save('kmeansclusters/' + str(i) + '.npy', kmeans.cluster_centers_)
-	np.save('kmeanslabels/' + str(i) + '.npy', kmeans.labels_)
+for i in range(2,10):
+    print(i)
+    for fold in range(0,folds):
+        temp = np.vstack((trX[0:fold*size_of_fold], trX[(fold + 1)*size_of_fold:len(trX)]))
+        print(temp.shape)
+        kmeans = KMeans(n_clusters=i, random_state=0).fit(temp)
+        np.save('kmeansclusters/' + str(i) + 'fold' + str(fold) + '.npy', kmeans.cluster_centers_)
+        np.save('kmeanslabels/' + str(i) + 'fold' + str(fold) + '.npy', kmeans.labels_)
